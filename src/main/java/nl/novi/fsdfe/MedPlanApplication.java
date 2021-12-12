ead41aabd59c1d81455350cbc2df33466a02b28c
bhvoetberg <b.h.voetberg@gmail.com>
2021-12-12 15:56:41 +0100

    POM voorzien van Springboot dependencies Basic packages aangemaakt Application.properties gevuld PostgresDB voorbereid

---
+++ src/main/java/nl/novi/fsdfe/MedPlanApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.fsdfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedPlanApplication {
+
+    public static void main(String[] args) {SpringApplication.run(MedPlanApplication.class, args);
+        System.out.println("Hallo");
+    }
+
+}
---
+++ src/test/java/nl/novi/fsdfe/MedPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.fsdfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
