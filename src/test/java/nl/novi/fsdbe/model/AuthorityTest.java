9e1e9130f2c89f530901fa07e1e7725e82b8cf92
bhvoetberg <b.h.voetberg@gmail.com>
2022-02-01 16:14:27 +0100

    Tests er op aangepast + opruimen POM

--- src/test/java/nl/novi/fsdbe/model/AuthorityTest.java
+++ src/test/java/nl/novi/fsdbe/model/AuthorityTest.java
@@ -1,11 +1,13 @@
 package nl.novi.fsdbe.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AuthorityTest {
 
     @Test
+    @DisplayName("Geeft Frits terug als username")
     public void setUsernameEqualsGetUsername() {
         Authority authority = new Authority();
         authority.setUsername("Frits");
@@ -13,6 +15,7 @@ class AuthorityTest {
     }
 
     @Test
+    @DisplayName("Geeft ROLE_USER terug")
     void setAuthorityEqualsGetAuthority() {
         Authority authority = new Authority();
         authority.setAuthority("ROLE_USER");
