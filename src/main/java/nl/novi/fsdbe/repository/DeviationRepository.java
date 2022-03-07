4f9ea7c09cf2e33a6ba846e46be2791aad558fb3
bhvoetberg <b.h.voetberg@gmail.com>
2022-03-07 14:46:41 +0100

    Unused import statement Repositories verwijderd

--- src/main/java/nl/novi/fsdbe/repository/DeviationRepository.java
+++ src/main/java/nl/novi/fsdbe/repository/DeviationRepository.java
@@ -2,7 +2,6 @@ package nl.novi.fsdbe.repository;
 
 import nl.novi.fsdbe.model.Deviation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface DeviationRepository extends JpaRepository<Deviation, Long> {
 }
