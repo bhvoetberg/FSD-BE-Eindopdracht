f22a82a981297e43daedf843aea18ce4392133c4
bhvoetberg <b.h.voetberg@gmail.com>
2022-05-09 16:30:51 +0200

    ClientServiceTest OK

--- src/test/java/nl/novi/fsdbe/service/ClientServiceTest.java
+++ src/test/java/nl/novi/fsdbe/service/ClientServiceTest.java
@@ -2,19 +2,22 @@ package nl.novi.fsdbe.service;
 
 import nl.novi.fsdbe.model.Client;
 import nl.novi.fsdbe.repository.ClientRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,22 +28,47 @@ class ClientServiceTest {
     @InjectMocks
     private ClientService clientService;
 
-//    @Captor
-//    ArgumentCaptor<Client> argumentCaptor;
+    private Client client1;
+    private Client client2;
+    private Client client3;
+    private List<Client> clientList;
 
-    @Test
-    @DisplayName("Should find 3 clients, 1 lastname Jansen")
-    void getClients() {
-        List<Client> clientList = new ArrayList<>();
-        Client client1 = new Client();
-        Client client2 = new Client();
-        Client client3 = new Client();
+    @BeforeEach
+    public void setup() {
+        this.client1 = new Client();
+        this.client2 = new Client();
+        this.client3 = new Client();
+
+        client1.setId(123L);
         client1.setLastName("Jansen");
+
+        client2.setFirstName("Mark");
         client2.setLastName("De Vries");
-        client3.setLastName("Boer");
+        client2.setRoomNumber("123");
+
+        client3.setSeeOwnMedication(true);
+
+        this.clientList = new ArrayList<>();
         clientList.add(client1);
         clientList.add(client2);
         clientList.add(client3);
+    }
+
+    @Test
+    @DisplayName("Should return client2 & client3 details")
+    public void shouldReturnClient2Arguments() {
+        assertEquals("Mark",client2.getFirstName());
+        assertEquals("De Vries", client2.getLastName());
+        assertEquals("123", client2.getRoomNumber());
+        assertTrue(client3.isSeeOwnMedication());
+    }
+
+
+
+
+    @Test
+    @DisplayName("Should find 3 clients, first lastname Jansen")
+    void getClients() {
 
         when(clientRepository.findAll()).thenReturn(clientList);
 
@@ -52,9 +80,12 @@ class ClientServiceTest {
     }
 
     @Test
-    public void shouldSaveNewClient() {
-        Client client = new Client();
-
+    @DisplayName("Should save client1 with ID 123L")
+    public void shouldSaveClients() {
+        given(clientRepository.findById(123L)).willReturn(Optional.of(client1));
+        Client savedClient = clientService.getClient(client1.getId());
+        assertThat(savedClient).isNotNull();
+        assertThat(savedClient.getId()).isEqualTo(client1.getId());
     }
 
 }
