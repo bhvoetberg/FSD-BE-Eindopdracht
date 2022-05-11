9fffab58a266959af2e2315352a0fa6c0c154260
bhvoetberg <b.h.voetberg@gmail.com>
2022-05-11 16:53:58 +0200

    Cleaned en tests nog steeds OK

--- src/test/java/nl/novi/fsdbe/end_to_end/PlanningTest.java
+++ src/test/java/nl/novi/fsdbe/end_to_end/PlanningTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest(classes = MedPlanApplication.class)
 @AutoConfigureMockMvc
